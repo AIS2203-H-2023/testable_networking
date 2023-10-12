e7ebeee26398ab87ec50330945b0e0f825797280
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2023-10-12 19:10:32 +0200

    improve test

--- tests/helper_test.cpp
+++ tests/helper_test.cpp
@@ -3,10 +3,18 @@
 
 #include "network_helper.hpp"
 
-TEST_CASE("Test network helper") {
+TEST_CASE("Test network helper [Big endian]") {
 
-    int i = 10;
-    auto bytes = int_to_bytes(i);
-    CHECK(i == bytes_to_int(bytes));
+    for (int i = -100; i < 100; i++) {
+        auto bytes = int_to_bytes(i, byte_order::BIG);
+        CHECK(i == bytes_to_int(bytes, byte_order::BIG));
+    }
+}
 
+TEST_CASE("Test network helper [Little endian]") {
+
+    for (int i = -100; i < 100; i++) {
+        auto bytes = int_to_bytes(i, byte_order::LITTLE);
+        CHECK(i == bytes_to_int(bytes, byte_order::LITTLE));
+    }
 }
